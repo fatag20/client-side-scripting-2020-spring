6bedc6a08094fc5e371809b794c3b11ae09c3a5b
Hubol Persson-Gordon <hubol.gordon@gmail.com>
2020-03-02 20:53:54 -0600

    .

--- quizzes/0/index.js
+++ quizzes/0/index.js
@@ -14,3 +14,21 @@ function alertMessage()
 {
     alert("Message");
 }
+
+function printSomeNumbers()
+{
+    const numbers = [0, 1, 3, 4];
+    numbers.forEach(number => console.log(number));
+}
+
+function returnSomeNumbers()
+{
+    const numbers = [0, 1, 3, 4, 6, 7, 8];
+    return numbers.filter(x => x % 2 === 0);
+}
+
+function doSomethingMysterious()
+{
+    const headerElement = document.getElementById("title");
+    headerElement.textContent = "Something";
+}

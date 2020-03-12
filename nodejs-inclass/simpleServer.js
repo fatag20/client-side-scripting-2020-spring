7d22718aa8d904e710ee258af531f43b762ef2f7
Hubol Persson-Gordon <hubol.gordon@gmail.com>
2020-03-12 08:03:59 -0500

    .

--- nodejs-inclass/simpleServer.js
+++ nodejs-inclass/simpleServer.js
@@ -1,14 +1,33 @@
 const http = require("http");
 
+const port = 8080;
+
 function allowAnyOrigin(res)
 {
     res.setHeader("Access-Control-Allow-Origin", "*");
+}
+
+function toReadableRequest(req)
+{
+    return `Request(${req.method} ${req.url})`;
 }
 
 let numberOfRequests = 0;
 
 http.createServer(( req, res ) => {
     allowAnyOrigin(res);
-    res.write(`Good morning Sam #${++numberOfRequests}!!!!`);
+    console.log(`Handling ${toReadableRequest(req)}`);
+
+    if (req.url !== "/")
+    {
+        res.writeHead(404);
+    }
+    else
+    {
+        res.write(`Good morning Sam #${++numberOfRequests}!!!!`);
+    }
+    
     res.end();
-}).listen(8080);
+}).listen(port);
+
+console.log(`Running on port ${port}`);
